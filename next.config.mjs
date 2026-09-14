d101a93ff61cb5ab9ad39d2e6975a39645580a22
arshad1808-dev <arshad@objectsquare.com>
2026-09-14 17:20:56 +0530

    .next build arror solved

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 import { fileURLToPath } from "node:url";
-/** @type {import('next').NextConfig} */
+
 const nextConfig = {
   outputFileTracingRoot: fileURLToPath(new URL(".", import.meta.url)),
-  distDir: process.env.NODE_ENV === "production" ? ".next-production" : ".next",
   poweredByHeader: false,
   images: {
     remotePatterns: [
